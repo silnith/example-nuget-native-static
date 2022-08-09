5768940c9cc68098845641f302da7e6a086026d1
silnith <silnith@gmail.com>
2022-08-09 08:39:35 -0700

    Make unit test a tad more understandable.

--- silnith.tessellation.static.tests/tesselator_tests.cpp
+++ silnith.tessellation.static.tests/tesselator_tests.cpp
@@ -16,10 +16,10 @@ namespace silnith::tessellation::tests
 			// arrange
 			tessellator const tessellator{};
 
-			vertex const bottom_left{ 10, 10, 0, 1 };
-			vertex const top_left{ 20, 21, 0, 1 };
-			vertex const top_right{ 31, 31, 0, 1 };
-			vertex const bottom_right{ 41, 40, 0, 1 };
+			vertex const bottom_left{ 0, 0, 0 };
+			vertex const top_left{ 0, 1, 0 };
+			vertex const top_right{ 1, 1, 0 };
+			vertex const bottom_right{ 1, 0, 0 };
 
 			polygon const square{ bottom_left, top_left, top_right, bottom_right };
 
@@ -29,13 +29,13 @@ namespace silnith::tessellation::tests
 			// assert
 			Assert::AreEqual(static_cast<std::vector<polygon>::size_type>(2), result.size());
 
-			std::vector<vertex> const first_vertices{ result[0].get_vertices() };
+			std::vector<vertex> const first_polygon_vertices{ result[0].get_vertices() };
 
-			Assert::AreEqual(static_cast<std::vector<vertex>::size_type>(3), first_vertices.size());
+			Assert::AreEqual(static_cast<std::vector<vertex>::size_type>(3), first_polygon_vertices.size());
 
-			vertex const one{ first_vertices[0] };
-			vertex const two{ first_vertices[1] };
-			vertex const three{ first_vertices[2] };
+			vertex const one{ first_polygon_vertices[0] };
+			vertex const two{ first_polygon_vertices[1] };
+			vertex const three{ first_polygon_vertices[2] };
 
 			Assert::AreEqual(bottom_left.get_x(), one.get_x(), epsilon);
 			Assert::AreEqual(bottom_left.get_y(), one.get_y(), epsilon);
@@ -49,13 +49,13 @@ namespace silnith::tessellation::tests
 			Assert::AreEqual(top_right.get_y(), three.get_y(), epsilon);
 			Assert::AreEqual(top_right.get_z(), three.get_z(), epsilon);
 
-			std::vector<vertex> const second_vertices{ result[1].get_vertices() };
+			std::vector<vertex> const second_polygon_vertices{ result[1].get_vertices() };
 
-			Assert::AreEqual(static_cast<std::vector<vertex>::size_type>(3), second_vertices.size());
+			Assert::AreEqual(static_cast<std::vector<vertex>::size_type>(3), second_polygon_vertices.size());
 
-			vertex const four{ second_vertices[0] };
-			vertex const five{ second_vertices[1] };
-			vertex const six{ second_vertices[2] };
+			vertex const four{ second_polygon_vertices[0] };
+			vertex const five{ second_polygon_vertices[1] };
+			vertex const six{ second_polygon_vertices[2] };
 
 			Assert::AreEqual(bottom_left.get_x(), four.get_x(), epsilon);
 			Assert::AreEqual(bottom_left.get_y(), four.get_y(), epsilon);
